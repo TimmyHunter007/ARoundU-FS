3a0213adabee6296723295acc105a87c6d1b6c61
TimothyHunter007 <twhunter@student.fullsail.edu>
2025-01-25 22:12:17 -0500

    Added an express rate limiters to limit amount of calls

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
@@ -11,6 +12,13 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files (CSS, JS, HTML)
+
+// Rate Limit
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {error: "Too many requests"},
+});
 
 // Routes
 app.use('/api/events', require('./routes/eventRoutes'));
