0978c760ec192ed27343671b3ce4d241ac77ba2b
TimothyHunter007 <twhunter@student.fullsail.edu>
2025-01-25 22:23:56 -0500

    Updating connection string to MongoDB to get local test env working.

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>
-{
-    try
-    {
-        await mongoose.connect(process.env.MONGODB_URI,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected');
-    } 
-    catch (error)
-    {
-        console.error(error.message);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {});
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
     }
 };
