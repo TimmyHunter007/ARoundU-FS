9a86fd2956ae576666a0bc297dff814258897d91
TimothyHunter007 <twhunter@student.fullsail.edu>
2025-02-06 21:40:09 -0500

    Updated style of the site.

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,38 +1,3 @@
-/*
-const axios = require('axios');
-
-const getEvents = async (req, res) => 
-{
-    const { location } = req.query;
-    if (!location) 
-    {
-        return res.status(400).json({ error: 'Location is required' });
-    }
-    try 
-    {
-        const response = await axios.get('https://www.eventbriteapi.com/v3/events/search/', 
-        {
-            headers: 
-            {
-                Authorization: `Bearer ${process.env.EVENTBRITE_API_KEY}`, // Use your token
-            },
-            params: 
-            {
-                'location.latitude': location.split(',')[0],
-                'location.longitude': location.split(',')[1],
-            },
-        });
-        res.status(200).json(response.data);
-    } 
-    catch (error) 
-    {
-        console.error('Error fetching events:', error.message);
-        res.status(error.response?.status || 500).json({ error: error.message });
-    }
-};
-
-module.exports = { getEvents };
-*/
 const axios = require('axios');
 require('dotenv').config(); // Load environment variables
 
