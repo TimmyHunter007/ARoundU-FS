8dd0030c909e81fc0d8c8f71d53ec8ae610d64d0
TimothyHunter007 <twhunter@student.fullsail.edu>
2025-02-21 15:37:49 -0500

    Updated connection string in db.js to force use the aroundU database.

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {});
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'aroundU',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
