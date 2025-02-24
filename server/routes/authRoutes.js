b3bbbb8caaeafd5fdf8dee97d3a01040e2dc83c2
TimothyHunter007 <twhunter@student.fullsail.edu>
2025-02-24 13:01:24 -0500

    Fixing a casting issue

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authController-temp');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Public routes
